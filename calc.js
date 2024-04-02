3a9fcfcb31a1b320a4c8fcfd4985440b937c56ac
arin-s <sarkisianarin@gmail.com>
2024-04-02 19:54:46 +1100

    Commit early and often, right? right....

---
+++ calc.js
@@ -0,0 +1,80 @@
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+let regex = /([0-9]*[.]?[0-9]*)?([^0-9.])?/g;
+let BLACKLIST = [" ", "", undefined];
+let WHITELIST = ["+", "-", "*", "/"];
+
+function operate(leftVar, rightVar, opReg, rightNeg, leftNeg) {
+  leftVar = parseInt(leftVar);
+  rightVar = parseInt(rightVar);
+  if (leftNeg)
+    leftVar *= -1;
+  if (rightNeg)
+    rightVar *= -1;
+  switch (opReg) {
+    case "+":
+      return leftVar + rightVar;
+    case "-":
+      return leftVar - rightVar;
+    case "*":
+      return leftVar * rightVar;
+    case "/":
+      return leftVar / rightVar;
+  }
+}
+
+function parse() {
+  let invalidTokens = [];
+  Array.from(input.value).forEach((token) => {
+    if (isNaN(token) && !WHITELIST.includes(token))
+      invalidTokens.push(token);
+  });
+  if (invalidTokens.length != 0) {
+    alert(`Invalid tokens: ${invalidTokens.toString()}`);
+    return;
+  }
+  let groups = input.value.split(regex);
+  groups = groups.filter((str) => !BLACKLIST.includes(str));
+  let leftVar = null, rightVar = null, opReg = null, rightNeg = false, leftNeg = false;
+  for (token of groups) {
+    if (!isNaN(token)) {
+      if (leftVar === null) {
+        leftVar = token;
+      } else if (opReg !== null) {
+        rightVar = token;
+        //Right register filled, do operation then clean
+        leftVar = operate(leftVar, rightVar, opReg, rightNeg, leftNeg);
+        rightVar = null, opReg = null, rightNeg = false, leftNeg = false;
+      } else {
+        alert("SYNTAX ERROR, ABORTING\nReason: Numbers not connected w/ operator");
+        return;
+      }
+    } else if (typeof token === "string") {
+      if (token === "-") {
+        if (leftVar === null) {
+          leftNeg = !leftNeg;
+        } else if (opReg === null) {
+          opReg = token;
+        } else {
+          rightNeg = !rightNeg;
+        }
+      } else if (leftVar !== null && opReg === null) {
+        opReg = token;
+      } else if (leftVar === null) {
+        alert(`SYNTAX ERROR, ABORTING\nReason: "${token}" is missing a number on the left side`);
+        return
+      } else if (opReg !== null) {
+        alert("SYNTAX ERROR, ABORTING\nReason: Two operators next to each other");
+        return;
+      }
+    } else {
+      alert("SYNTAX ERROR, ABORTING\nReason: Unknown token type");
+      return;
+    }
+  }
+  if (rightVar !== null || opReg !== null || leftNeg || rightNeg) {
+    alert("SYNTAX ERROR, ABORTING\nReason: End of expression not complete");
+    return;
+  }
+  output.innerText = leftVar;
+}
